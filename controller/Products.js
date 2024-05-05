d179880789134b6da64d4c67b57a8477e62282da
ashaansojib <ashaduzzamansojib67@gmail.com>
2024-05-05 16:57:06 +0600

    set limit and paginaiton

--- controller/Products.js
+++ controller/Products.js
@@ -4,23 +4,46 @@ const errorResponse = require("../utils/errorResponse");
 
 // @title: get all products, route: /api/products, access: public
 exports.getProducts = asyncHandler(async (req, res, next) => {
-
-  if (req.query) {
+  if (req.query.category) {
     const queryData = await pcpartsbdProducts.find(req.query);
     return res
       .status(200)
-      .json({ success: true, data: queryData, count: queryData.length });
+      .json({ success: true, count: queryData.length, data: queryData });
   }
 
-  const products = await pcpartsbdProducts.find();
-  res
-    .status(200)
-    .json({ success: true, data: products, count: products.length });
+  // pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 10;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await pcpartsbdProducts.countDocuments();
+
+  const query = await pcpartsbdProducts.find().skip(startIndex).limit(limit);
+  // pagination result
+  const pagination = {};
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+  const products = await query;
+  res.status(200).json({
+    success: true,
+    count: products.length,
+    pagination,
+    data: products,
+  });
 });
 
 // @title: create products, route: /api/products, access: privet
 exports.createProduct = asyncHandler(async (req, res, next) => {
-
   const product = req.body;
   const add = await pcpartsbdProducts.create(product);
 
@@ -29,7 +52,6 @@ exports.createProduct = asyncHandler(async (req, res, next) => {
 
 // @title: remove single product, route: /api/products/:id, access: delete
 exports.singleProduct = asyncHandler(async (req, res, next) => {
-
   const id = req.params.id;
   const product = await pcpartsbdProducts.findOne({ _id: id });
 
@@ -38,7 +60,6 @@ exports.singleProduct = asyncHandler(async (req, res, next) => {
 
 // @title: remove single product, route: /api/products/:id, access: delete
 exports.removeProduct = asyncHandler(async (req, res, next) => {
-
   const id = req.params.id;
   const remove = await pcpartsbdProducts.deleteOne({ _id: id });
 
